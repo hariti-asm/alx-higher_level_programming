3a12bda2eec33107144d9a18442409f9d53efc5e
hariti-asm <haritiasmaa74.com>
2024-01-03 15:17:44 -0800

    .

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,5 @@
 #!/usr/bin/node
-// import module fs to read a file
-
 const fs = require('fs');
-
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', (error, data) => {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log(data);
-  }
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
